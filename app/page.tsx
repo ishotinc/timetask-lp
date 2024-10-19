99ee6f31646674ceb80a4a6ae75c36db92977b75
stargazer1846923 <kondorymansat@gmail.com>
2024-10-19 13:52:02 +0900

    フォルダ名変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { LandingPageComponent } from "@/components/landing-page";
+import { LandingPageComponent } from "@/app/_components/landing-page";
 
 export default function Page() {
   return <LandingPageComponent />;
